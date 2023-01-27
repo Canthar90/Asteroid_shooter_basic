0a5e269cbf69fbabcd565aa617e8c0977a493e4d
Canthar90 <d.cieslak90@gmail.com>
2023-01-27 15:02:25 +0100

    Added laser rectangle

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ ship_surf = pygame.image.load("graphics\spaceship.png").convert_alpha()
 ship_surf = pygame.transform.scale(ship_surf, (80,90))
 ship_rect = ship_surf.get_rect(midbottom=(WINOW_WIDTH/2, WINDOW_HEIGHT-100))
 print(ship_rect)
-ship_y_pos = 600
+
+laser_surf = pygame.image.load("graphics\laser.png").convert_alpha()
+laser_surf = pygame.transform.rotate(laser_surf, angle=90)
+laser_surf = pygame.transform.scale(laser_surf, (35,45))
+laser_rect = laser_surf.get_rect(midbottom=(ship_rect.midtop[0] + 90, ship_rect.midtop[1]))
 
 background_surf = pygame.image.load("graphics\galaxy.jpg").convert_alpha()
 background_surf = pygame.transform.scale(background_surf, (WINOW_WIDTH, WINDOW_HEIGHT))
@@ -23,8 +27,10 @@ font =  pygame.font.Font("graphics\subatomic.tsoonami.ttf" ,50)
 text_surf = font.render("Spaaceeee", True,"White")
 text_rect = text_surf.get_rect(midbottom=(WINOW_WIDTH/2, WINDOW_HEIGHT - 20))
 
-pygame.display.set_caption("Asteroid shooter V1")
 
+
+pygame.display.set_caption("Asteroid shooter V1")
+shoot_flag = False
 while True:
     
     # input
@@ -33,17 +39,12 @@ while True:
             pygame.quit()
             sys.exit()
             
-        # if event.type == pygame.MOUSEMOTION:
-        #     move_ship_pos = event.pos[0]
-            
-                    
-        # if event.type == pygame.MOUSEBUTTONDOWN:
-        #     print("shoot")
-    
     
     # mouse position
     move_ship_pos = pygame.mouse.get_pos()[0]
     mouse_buttons = pygame.mouse.get_pressed()
+    
+    
     
     if mouse_buttons[0]:
         print("shoot")
@@ -62,7 +63,8 @@ while True:
             else:
                 ship_rect.centerx -= 1
             
-
+    if shoot_flag:
+        laser_rect.y -= 9
     
     # framerate limit
     clock.tick(120)
@@ -70,10 +72,17 @@ while True:
     # updates
     display_surface.blit(background_surf,(0,0))
     # display_surface.fill((0, 0, 0))
-    display_surface.blit(ship_surf, ship_rect)
+    
     display_surface.blit(text_surf, text_rect)
+    pygame.draw.rect(display_surface, (255, 255, 255), text_rect.inflate(25,25), 7, 5)
     
+    if mouse_buttons[0] or shoot_flag:
+        
+        print("passed")
+        display_surface.blit(laser_surf, laser_rect)
+        shoot_flag = True
     
+    display_surface.blit(ship_surf, ship_rect)
     
     # show the frame to the player / update display surface
     pygame.display.update()
