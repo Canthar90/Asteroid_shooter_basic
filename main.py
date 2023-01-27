7200f7c40767ec34b4a20ed08ae65424110761ba
Canthar90 <d.cieslak90@gmail.com>
2023-01-27 10:51:47 +0100

    Added backgroun image ship image text and first movement

--- main.py
+++ main.py
@@ -4,10 +4,21 @@ import sys
 
 pygame.init()
 WINOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+clock = pygame.time.Clock()
 
 display_surface = pygame.display.set_mode((WINOW_WIDTH, WINDOW_HEIGHT))
 
-test_surf = pygame.Surface((400, 100))
+# importing images 
+ship_surf = pygame.image.load("graphics\spaceship.png").convert_alpha()
+ship_surf = pygame.transform.scale(ship_surf, (80,90))
+ship_y_pos = 600
+
+background_surf = pygame.image.load("graphics\galaxy.jpg").convert_alpha()
+background_surf = pygame.transform.scale(background_surf, (WINOW_WIDTH, WINDOW_HEIGHT))
+
+# import text
+font =  pygame.font.Font("graphics\subatomic.tsoonami.ttf" ,50)
+text_surf = font.render("Spaaceeee", True,"White")
 
 pygame.display.set_caption("Asteroid shooter V1")
 
@@ -18,11 +29,20 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+            
+        # if event.type == pygame.
+    
+    # framerate limit
+    clock.tick(120)
     
     # updates
-    display_surface.fill((34, 34, 54))
-    display_surface.blit(test_surf, (100,0))
-    test_surf.fill((173, 59, 17))
+    display_surface.blit(background_surf,(0,0))
+    # display_surface.fill((0, 0, 0))
+    display_surface.blit(ship_surf,(300,ship_y_pos))
+    ship_y_pos -= 2
+    display_surface.blit(text_surf, (WINOW_WIDTH/2 -150, WINDOW_HEIGHT-50))
+    
+    
     
     # show the frame to the player / update display surface
     pygame.display.update()
